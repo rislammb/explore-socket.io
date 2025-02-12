762fd1f15ee1ca2d0973f85a7f22c521a7320d1a
Rayhanul Islam <rislammb@gmail.com>
2025-02-12 22:20:24 +0600

    update with server delivery for get missed messages

--- index.js
+++ index.js
@@ -3,10 +3,27 @@ import { createServer } from "node:http";
 import { fileURLToPath } from "node:url";
 import { dirname, join } from "node:path";
 import { Server } from "socket.io";
+import { open } from "sqlite";
+import sqlite3 from "sqlite3";
+
+const db = await open({
+  filename: "chat.db",
+  driver: sqlite3.Database,
+});
+
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    client_offset TEXT UNIQUE,
+    content TEXT
+  );
+`);
 
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+  connectionStateRecovery: {},
+});
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get("/", (_req, res) => {
@@ -16,17 +33,29 @@ app.get("/", (_req, res) => {
 io.on("connection", async (socket) => {
   console.log("a user connected");
 
-  socket.on("chat message", (msg) => {
-    io.emit("chat message", msg);
+  socket.on("chat message", async (msg) => {
+    let result;
+    try {
+      result = await db.run("INSERT INTO messages (content) VALUES (?)", msg);
+    } catch (error) {
+      console.log(error);
+    }
+
+    io.emit("chat message", msg, result.lastID);
   });
 
-  try {
-    const response = await socket
-      .timeout(3000)
-      .emitWithAck("request", { foo: "bar" }, "baz");
-    console.log(response.status);
-  } catch (err) {
-    console.log("Error:", err);
+  if (!socket.recovered) {
+    try {
+      await db.each(
+        `SELECT id, content FROM messages WHERE id > ?`,
+        [socket.handshake.auth.serverOffset || 0],
+        (_err, row) => {
+          socket.emit("chat message", row.content, row.id);
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   socket.on("disconnect", () => {
